a3f8b3dd0387d8ae8a56afb385113d5c8794eae4
Wenbin Fang <wenbin@listennotes.com>
2022-10-21 19:10:36 -0700

    Update

--- functions/api/r2-ops.js
+++ functions/api/r2-ops.js
@@ -1,4 +1,4 @@
-// import * as AWS from 'aws-sdk';
+import * as AWS from 'aws-sdk';
 
 async function _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, region, bucket, key) {
   const s3 = new AWS.S3({
