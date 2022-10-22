2b11dfca1cf9de16e739e1f73bbe4276461c82b6
Wenbin Fang <wenbin@listennotes.com>
2022-10-21 20:10:28 -0700

    Update

--- functions/api/r2-ops.js
+++ functions/api/r2-ops.js
@@ -1,12 +1,12 @@
 import { AwsClient } from 'aws4fetch'
 
-async function _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, region, bucket, key) {
+async function _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, region) {
 
   const aws = new AwsClient({
     accessKeyId,
     secretAccessKey,
-    service: 's3',
-    'region': 'auto',
+    'service': 's3',
+    region,
   });
 
   const request = new Request(endpoint, {
@@ -21,22 +21,22 @@ async function getPresignedUrlFromR2(env, bucket, key) {
   const accessKeyId = `${env.ACCESS_KEY_ID}`
   const secretAccessKey = `${env.SECRET_ACCESS_KEY}`;
   const endpoint = `https://${bucket}.${env.ACCOUNT_ID}.r2.cloudflarestorage.com/${key}`;
-  return _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, 'auto', bucket, key);
+  return _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, 'auto');
 }
 
 async function getPresignedUrlFromS3(env, bucket, key) {
   const accessKeyId = `${env.AWS_ACCESS_KEY_ID}`
   const secretAccessKey = `${env.AWS_SECRET_ACCESS_KEY}`;
   const region = 'us-west-2';
-  const endpoint = `https://s3.${region}.amazonaws.com`;
-  return _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, region, bucket, key);
+  const endpoint = `https://${bucket}.s3.${region}.amazonaws.com/${key}`;
+  return _getPresignedUrl(accessKeyId, secretAccessKey, endpoint, region);
 }
 
 export async function onRequestPost({request, env}) {
   const inputParams = await request.json();
-  const url = await getPresignedUrlFromR2(env, 'tmp-files', inputParams.name);
+  const url = await getPresignedUrlFromR2(env, env.BUCKET, inputParams.name);
 
-  // const url = await getPresignedUrlFromS3(env, 'temp-data-listennotes-com', inputParams.name);
+  // const url = await getPresignedUrlFromS3(env, env.AWS_BUCKET, inputParams.name);
   const retData = {
     url,
   }
