07c63c1ece02c5e732c97052c2da489156248854
Wenbin Fang <wenbin@listennotes.com>
2022-10-27 13:40:11 -0700

    Update

--- client-src/ClientAdminPodcastApp/components/EditPodcastApp/index.jsx
+++ client-src/ClientAdminPodcastApp/components/EditPodcastApp/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Requests from '../../../common/requests';
 import AdminNavApp from '../../../components/AdminNavApp';
+import AdminImageUploaderApp from '../../../components/AdminImageUploaderApp';
+
+const SUBMIT_STATUS__START = 1;
 
 export default class EditPodcastApp extends React.Component {
   constructor(props) {
@@ -14,10 +17,8 @@ export default class EditPodcastApp extends React.Component {
 
     this.state = {
       feed,
+      submitStatus: null,
     }
-  }
-
-  componentDidMount() {
   }
 
   onUpdateFeed(props) {
@@ -36,27 +37,44 @@ export default class EditPodcastApp extends React.Component {
   onSubmit(e) {
     e.preventDefault();
     const {feed} = this.state;
+    this.setState({submitStatus: SUBMIT_STATUS__START});
     Requests.post('/admin/ajax/feed/', feed)
-      .then((data) => console.log(data));
+      .then((data) => {
+        this.setState({submitStatus: null});
+        console.log(data);
+      });
   }
 
   render() {
-    const {feed} = this.state;
+    const {feed, submitStatus} = this.state;
+    const submitting = submitStatus === SUBMIT_STATUS__START;
     return (<AdminNavApp>
-      <form>
-        <label>
-          Podcast title
-          <div>
-            <input
-              type="text"
-              value={feed.title || ''}
-              onChange={(e) => this.onUpdatePodcastMeta(e, 'title')}
-            />
+      <form className="lh-page-card mx-4 grid grid-cols-1 gap-4">
+        <div className="flex">
+          <div className="flex-none">
+            <AdminImageUploaderApp />
           </div>
-        </label>
-        <div>
-          <button type="submit" className="lh-btn lh-btn-brand-dark" onClick={this.onSubmit}>
-            Update
+          <div className="flex-1 ml-8">
+            <label>
+              Podcast title
+              <div>
+                <input
+                  type="text"
+                  value={feed.title || ''}
+                  onChange={(e) => this.onUpdatePodcastMeta(e, 'title')}
+                />
+              </div>
+            </label>
+          </div>
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="lh-btn lh-btn-brand-dark lh-btn-lg"
+            onClick={this.onSubmit}
+            disabled={submitting}
+          >
+            {submitting ? 'Updating...' : 'Update'}
           </button>
         </div>
       </form>
---
+++ client-src/components/AdminImageUploaderApp/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// import clsx from 'clsx';
+
+export default class AdminImageUploaderApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (<div className="">
+      image
+    </div>);
+  }
+}
