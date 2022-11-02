3d3e66315e60834348fc659a77144affe1680a5e
Wenbin Fang <wenbin@listennotes.com>
2022-11-01 19:38:51 -0700

    Update

--- client-src/common/ClientUrlUtils.js
+++ client-src/common/ClientUrlUtils.js
@@ -1,9 +1,7 @@
 export function getPublicBaseUrl() {
   const {location} = window;
   let baseUrl = `${location.protocol}//${location.hostname}`;
-  console.log(location.protocol);
-  if (!(location.port === '80' && location.protocol === 'http:') &&
-      !(location.port === '443' && location.protocol === 'https:')) {
+  if (location.port !== '') {
     baseUrl = `${baseUrl}:${location.port}`;
   }
   return baseUrl;
