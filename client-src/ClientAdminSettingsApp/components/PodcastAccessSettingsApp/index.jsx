f631a34cf7a3c5704d00013f3554a1f64fe5b8a1
Wenbin Fang <wenbin@listennotes.com>
2022-11-09 12:33:57 -0800

    Update

--- client-src/ClientAdminSettingsApp/components/PodcastAccessSettingsApp/index.jsx
+++ client-src/ClientAdminSettingsApp/components/PodcastAccessSettingsApp/index.jsx
@@ -6,9 +6,12 @@ import clsx from "clsx";
 function AccessOption({value, header, description}) {
   return (<RadioGroup.Option value={value}>
     {({checked}) => (
-      <div className={clsx(checked ? 'bg-blue-200' : '')}>
-        <div>{header}</div>
-        <div>{description}</div>
+      <div className={clsx('border p-2 hover:cursor-pointer',
+        checked ? 'bg-brand-light border-brand-light' : '')}>
+        <div className={clsx('text-sm font-semibold', checked ? 'text-white' : '')}>{header}</div>
+        <div className={clsx('text-xs', checked ? 'text-white' : 'text-muted-color')}>
+          {description}
+        </div>
       </div>
     )}
   </RadioGroup.Option>);
@@ -59,21 +62,23 @@ export default class PodcastAccessSettingsApp extends React.Component {
         value={access.currentPolicy}
         onChange={(value) => this.onUpdateAccess({currentPolicy: value})}
       >
-        <AccessOption
-          value="public"
-          header="Public"
-          description="something something"
-        />
-        <AccessOption
-          value="passcode"
-          header="Passcode"
-          description="something2 something"
-        />
-        <AccessOption
-          value="offline"
-          header="Offline"
-          description="something3 something"
-        />
+        <div className="grid grid-cols-1 gap-4">
+          <AccessOption
+            value="public"
+            header="Public"
+            description="Make the entire site publicly accessible."
+          />
+          {/*<AccessOption*/}
+          {/*  value="passcode"*/}
+          {/*  header="Passcode"*/}
+          {/*  description="something2 something"*/}
+          {/*/>*/}
+          <AccessOption
+            value="offline"
+            header="Offline"
+            description="Make the entire site offline. All web pages, rss feed and json feed will be 404-ed."
+          />
+        </div>
       </RadioGroup>
     </SettingsBase>);
   }
