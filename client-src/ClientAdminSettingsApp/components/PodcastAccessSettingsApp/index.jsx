33296362ce709f2e44c7eba08575639515525862
Wenbin Fang <wenbin@listennotes.com>
2022-11-09 12:23:16 -0800

    Boilerplate for access control

--- client-src/ClientAdminSettingsApp/components/PodcastAccessSettingsApp/index.jsx
+++ client-src/ClientAdminSettingsApp/components/PodcastAccessSettingsApp/index.jsx
@@ -1,17 +1,48 @@
 import React from 'react';
+import { RadioGroup } from '@headlessui/react'
 import SettingsBase from '../SettingsBase';
+import clsx from "clsx";
+
+function AccessOption({value, header, description}) {
+  return (<RadioGroup.Option value={value}>
+    {({checked}) => (
+      <div className={clsx(checked ? 'bg-blue-200' : '')}>
+        <div>{header}</div>
+        <div>{description}</div>
+      </div>
+    )}
+  </RadioGroup.Option>);
+}
 
 export default class PodcastAccessSettingsApp extends React.Component {
   constructor(props) {
     super(props);
+
+    this.onUpdateAccess = this.onUpdateAccess.bind(this);
+
     const currentType = 'access';
+    const {feed} = props;
+    let access = {currentPolicy: 'public', passcode: 'secret'};
+    if (feed.settings && feed.settings[currentType]) {
+      access = feed.settings[currentType];
+    }
     this.state = {
       currentType,
+      access,
     };
   }
 
+  onUpdateAccess(props) {
+    this.setState((prevState) => ({
+      access: {
+        ...prevState.access,
+        ...props,
+      },
+    }));
+  }
+
   render() {
-    const {currentType} = this.state;
+    const {currentType, access} = this.state;
     const {submitting, submitForType} = this.props;
     return (<SettingsBase
       title="Access control"
@@ -24,7 +55,26 @@ export default class PodcastAccessSettingsApp extends React.Component {
         // });
       }}
     >
-      access
+      <RadioGroup
+        value={access.currentPolicy}
+        onChange={(value) => this.onUpdateAccess({currentPolicy: value})}
+      >
+        <AccessOption
+          value="public"
+          header="Public"
+          description="something something"
+        />
+        <AccessOption
+          value="passcode"
+          header="Passcode"
+          description="something2 something"
+        />
+        <AccessOption
+          value="offline"
+          header="Offline"
+          description="something3 something"
+        />
+      </RadioGroup>
     </SettingsBase>);
   }
 }
