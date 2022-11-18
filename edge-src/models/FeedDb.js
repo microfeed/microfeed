ddd4073556a4141c038ef153f957eea5f778ff4a
Wenbin Fang <wenbin@listennotes.com>
2022-11-17 16:17:41 -0800

    Update

--- edge-src/models/FeedDb.js
+++ edge-src/models/FeedDb.js
@@ -1,7 +1,7 @@
-// import {projectPrefix} from "../../common-src/R2Utils";
-// import {buildAudioUrlWithTracking, PUBLIC_URLS} from "../../common-src/StringUtils";
-// import {ITEM_STATUSES} from "../../common-src/Constants";
-
+/**
+ * XXX: d1 binding to env not working for now
+ * https://github.com/cloudflare/wrangler2/issues/2168
+ */
 export default class FeedDb {
   constructor(env) {
     this.FEED_DB = env.FEED_DB;
