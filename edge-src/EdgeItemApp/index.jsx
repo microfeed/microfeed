153300ad26b749cb070e5124630cd74fda31148c
Wenbin Fang <wenbin@listennotes.com>
2022-11-20 20:24:52 -0800

    Update

--- edge-src/EdgeItemApp/index.jsx
+++ edge-src/EdgeItemApp/index.jsx
@@ -15,16 +15,16 @@ export default class EdgeItemApp extends React.Component {
         webpackCssList={[]}
         canonicalUrl={canonicalUrl}
         favicon={{
-          'apple-touch-icon': '/assets/apple-touch-icon.png',
-          '32x32': '/assets/favicon-32x32.png',
-          '16x16': '/assets/favicon-16x16.png',
-          'manifest': '/assets/site.webmanifest',
-          'mask-icon': {
-            'href': '/assets/safari-pinned-tab.svg',
-            'color': '#b82f00',
-          },
-          'msapplication-TileColor': '#da532c',
-          'theme-color': '#ffffff',
+          // 'apple-touch-icon': '/assets/apple-touch-icon.png',
+          // '32x32': '/assets/favicon-32x32.png',
+          // '16x16': '/assets/favicon-16x16.png',
+          // 'manifest': '/assets/site.webmanifest',
+          // 'mask-icon': {
+          //   'href': '/assets/safari-pinned-tab.svg',
+          //   'color': '#b82f00',
+          // },
+          // 'msapplication-TileColor': '#da532c',
+          // 'theme-color': '#ffffff',
         }}
       />
       <body>
