1cf8224c4e70f4ce1b15c3f938d0fc45c88b81b4
Wenbin Fang <wenbin@listennotes.com>
2022-11-22 11:52:16 -0800

    Update

--- client-src/ClientAdminItemsApp/components/EditItemApp/index.jsx
+++ client-src/ClientAdminItemsApp/components/EditItemApp/index.jsx
@@ -81,7 +81,7 @@ export default class EditItemApp extends React.Component {
       [itemId]: {...item},
     };
     // XXX: delete later
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 100; i++) {
       itemsBundle[randomShortUUID()] = {...item};
     }
     // XXX: delete later
