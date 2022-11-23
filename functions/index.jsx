28b48dd3a55acdb59dfa8a14e9730167bea11ae7
Wenbin Fang <wenbin@listennotes.com>
2022-11-22 18:43:21 -0800

    Update

--- functions/index.jsx
+++ functions/index.jsx
@@ -8,6 +8,7 @@ export async function onRequestGet({env, request}) {
   const queryRes = await FEED_DB.prepare("SELECT * FROM items").all();
   console.log('query duration', queryRes.duration);
   console.log('records total', queryRes.results.length);
+  console.log('records[1]', queryRes.results[1]);
   return webResponseBuilder.getResponse({
     getComponent: (content, jsonData, theme) => {
       return <EdgeHomeApp jsonData={jsonData} theme={theme}/>;
