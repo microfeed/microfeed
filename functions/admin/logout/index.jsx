61cec8d0d2238ed6f9eb4b00732e2bf2ce58079f
Wenbin Fang <wenbin@listennotes.com>
2022-12-04 08:40:22 -0800

    Try logout

--- functions/admin/logout/index.jsx
+++ functions/admin/logout/index.jsx
@@ -1,7 +1,7 @@
 import {ADMIN_URLS} from "../../../common-src/StringUtils";
 
 export async function onRequestGet() {
-  return new Response(
+  const response = new Response(
     `<html><title>Logout</title><body>Logged out. <a href="${ADMIN_URLS.home()}">Login again?</a></body></html>`,
     {
       status: 401,
@@ -9,4 +9,7 @@ export async function onRequestGet() {
         'Content-Type': 'text/html; charset=utf-8',
       },
     });
+  const newResponse = new Response(response.body, response)
+  newResponse.headers.set('CF_Authorization', '');
+  return newResponse;
 }
