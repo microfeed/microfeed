99190e41f1012c8c45fb983cb41134fe9a15ce52
Wenbin Fang <wenbin@listennotes.com>
2022-12-12 07:40:02 +0900

    Refactor r2-ops to be more robust

--- client-src/common/requests.js
+++ client-src/common/requests.js
@@ -15,49 +15,44 @@ const requestPost = (url, bodyDict) => {
 
 async function uploadFile(file, cdnFilename, onProgress, onUploaded, onFailure) {
   const { size, type } = file;
-  const rawResponse = await fetch('/admin/ajax/r2-ops', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      size,
-      key: cdnFilename,
-      type,
-    })
+  requestPost('/admin/ajax/r2-ops', {
+    size,
+    key: cdnFilename,
+    type,
+  }).then((res) => {
+    const fileReader = new FileReader();
+    fileReader.onloadend = async (e) => {
+      const arrayBuffer = e.target.result;
+      if (arrayBuffer) {
+        const {mediaBaseUrl, presignedUrl} = res;
+        const xhr = new XMLHttpRequest();
+        xhr.open("PUT", presignedUrl, true);
+        xhr.upload.addEventListener("progress", (event) => {
+          if (event.lengthComputable) {
+            onProgress(event.loaded / event.total);
+            // this.setState({progressText: `${parseFloat(event.loaded / event.total * 100.0).toFixed(2)}%`});
+          }
+        });
+        xhr.addEventListener("loadend", () => {
+          const mediaUrl = `${mediaBaseUrl}/${cdnFilename}`;
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            // this.props.onImageUploaded(mediaUrl);
+            onUploaded(mediaUrl, arrayBuffer);
+          }
+          // this.setState({progressText: null, uploadStatus: null});
+        });
+        xhr.addEventListener("error", (event) => {
+          if (onFailure) {
+            onFailure(event);
+          }
+        });
+        xhr.send(arrayBuffer);
+      }
+    };
+    fileReader.readAsArrayBuffer(file);
+  }).catch((response) => {
+    console.log(response);
   });
-  const res = await rawResponse.json();
-  const fileReader = new FileReader();
-  fileReader.onloadend = async (e) => {
-    const arrayBuffer = e.target.result;
-    if (arrayBuffer) {
-      const {mediaBaseUrl, presignedUrl} = res;
-      const xhr = new XMLHttpRequest();
-      xhr.open("PUT", presignedUrl, true);
-      xhr.upload.addEventListener("progress", (event) => {
-        if (event.lengthComputable) {
-          onProgress(event.loaded / event.total);
-          // this.setState({progressText: `${parseFloat(event.loaded / event.total * 100.0).toFixed(2)}%`});
-        }
-      });
-      xhr.addEventListener("loadend", () => {
-        const mediaUrl = `${mediaBaseUrl}/${cdnFilename}`;
-        if (xhr.readyState === 4 && xhr.status === 200) {
-          // this.props.onImageUploaded(mediaUrl);
-          onUploaded(mediaUrl, arrayBuffer);
-        }
-        // this.setState({progressText: null, uploadStatus: null});
-      });
-      xhr.addEventListener("error", (event) => {
-        if (onFailure) {
-          onFailure(event);
-        }
-      });
-      xhr.send(arrayBuffer);
-    }
-  };
-  fileReader.readAsArrayBuffer(file);
 }
 
 const Requests = {
