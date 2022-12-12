7b3e9c7b2a91b8bc6f6cd0d10871606f47b65a35
Wenbin Fang <wenbin@listennotes.com>
2022-12-12 20:44:19 +0900

    Update

--- functions/admin/ajax/feed.js
+++ functions/admin/ajax/feed.js
@@ -5,10 +5,9 @@ export async function onRequestPost({request, env}) {
   const feed = new FeedDb(env, request);
   await feed.putContent(updatedFeed);
 
-  return Response.redirect('http://127.0.0.1:8788/');
-  // return new Response(JSON.stringify({}), {
-  //   headers: {
-  //     'content-type': 'application/json;charset=UTF-8',
-  //   },
-  // });
+  return new Response(JSON.stringify({}), {
+    headers: {
+      'content-type': 'application/json;charset=UTF-8',
+    },
+  });
 }
