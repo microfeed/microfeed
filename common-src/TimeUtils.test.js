fc7c96bb5c7af2b34423b0560aa533674b579fac
Wenbin Fang <wenbin@listennotes.com>
2023-02-07 10:26:21 -0800

    Fix unit tests.

--- common-src/TimeUtils.test.js
+++ common-src/TimeUtils.test.js
@@ -2,7 +2,7 @@ import {datetimeLocalStringToMs, msToDatetimeLocalString} from "./TimeUtils";
 
 test('datetimeStringToMs', () => {
   const dtStr = '2022-10-31T18:49';
-  const dtMs = 1667267340000;
+  const dtMs = 1667242140000;
   expect(datetimeLocalStringToMs(dtStr)).toBe(dtMs);
   expect(msToDatetimeLocalString(dtMs)).toBe(dtStr);
 });
