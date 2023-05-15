19060e29b81d627df1705a7703f057f3ed158045
Wenbin Fang <wenbin@listennotes.com>
2023-05-14 19:29:24 -0700

    Sync microfeed version in openapi.yaml

--- functions/json/openapi.yaml/index.jsx
+++ functions/json/openapi.yaml/index.jsx
@@ -1,11 +1,15 @@
 import OpenAPIYaml from '../../../edge-src/EdgeApiApp/openapi.yaml.html';
+import {MICROFEED_VERSION} from "../../../common-src/Version";
 
 const Mustache = require('mustache');
 
 export async function onRequestGet({request}) {
   const urlObj = new URL(request.url);
   const baseUrl = urlObj.origin;
-  const html = Mustache.render(OpenAPIYaml, {baseUrl});
+  const html = Mustache.render(OpenAPIYaml, {
+    baseUrl,
+    microfeedVersion: MICROFEED_VERSION
+  });
 
   return new Response(html, {
     headers: {
