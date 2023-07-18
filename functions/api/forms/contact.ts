4b00b9c49ff35a74c3ac3185f55580b4eb1fb91b
gnshd <gdev@nshd.info>
2023-07-18 13:49:26 +0900

    mailchannelsでプラグインを使わないように

--- functions/api/forms/contact.ts
+++ functions/api/forms/contact.ts
@@ -1,19 +1,59 @@
-import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+export const onRequest = async ({ request, env }) => {
+  const recipientEmail = env.RECEIVER_EMAIL; // 自分のメールアドレス
+  const sender = env.SENDER_NAME; // 自分の名前や会社名
+  const senderEmail = env.SENDER_EMAIL; // 送信に使うメールアドレス（ドメインが同じなら適当でも可）
+  const formData = await request.formData(); // クライアントサイドから送られたフォームデータを取得
+  const name = formData.get("name"); // フォームデータの中身
+  const email = formData.get("email"); // フォームデータの中身
+  const detail = formData.get("detail"); // フォームデータの中身
 
-export const onRequest: PagesFunction = (context) =>
-  mailChannelsPlugin({
-    personalizations: [
-      {
-        to: [{ name: "ACME Support", email: `${context.env.RECEIVER_EMAIL}` }],
+  const toAdminRes = await fetch("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [{ email: recipientEmail }],
+        },
+      ],
+      from: { email: senderEmail, name: sender },
+      subject: "お問い合わせがありました",
+      content: [
+        {
+          type: "text/plain",
+          value: `【お名前】\n${name}様\n【メールアドレス】\n${email}\n【お問い合わせ内容】\n${detail}`,
+        },
+      ],
+    }),
+  });
+
+  if (toAdminRes.ok) {
+    await fetch("https://api.mailchannels.net/tx/v1/send", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
       },
-    ],
-    from: {
-      name: "Contact Us Form Inquiry",
-      email: `${context.env.SENDER_EMAIL}`,
-    },
-    respondWith: () => {
-      return new Response(
-        `Thank you for submitting your inquiry. A member of the team will be in touch shortly.`
-      );
-    },
-  })(context);
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [{ email }],
+          },
+        ],
+        from: { email: senderEmail, name: sender },
+        subject: "お問い合わせありがとうございます",
+        content: [
+          {
+            type: "text/plain",
+            value: `${name}様\nこの度はお問い合わせいただきありがとうございます。(略)\n【お問い合わせ内容】\n${detail}`,
+          },
+        ],
+      }),
+    });
+
+    return new Response("送信に成功しました！", { status: 200 });
+  }
+
+  return new Response("送信に失敗しました。", { status: 500 });
+};
