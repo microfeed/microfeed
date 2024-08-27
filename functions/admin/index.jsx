c5793bd42cc2c4c87fc630b1d9f6d6aa3418b4d9
Angel Abad Cerdeira <angel@abadcer.com>
2024-08-27 21:09:39 +1000

    protecting /admin and style fixes

--- functions/admin/index.jsx
+++ functions/admin/index.jsx
@@ -5,6 +5,15 @@ import EdgeAdminHomeApp from "../../edge-src/EdgeAdminHomeApp";
 export async function onRequestGet({ data }) {
   const {feedContent, onboardingResult} = data;
 
+  // abadcer-com - 403 for /admin pages if host is Cloudflare's pages.dev
+  const url = data.feedDb.request.url;
+  const baseUrl = data.feedDb.baseUrl;
+  // Check if the URL contains /admin
+  if (url.includes('/admin') && baseUrl.includes('pages.dev')) {
+    return new Response('Forbidden', { status: 403 });
+  }
+  // abadcer-com END
+
   const fromReact = renderReactToHtml(<EdgeAdminHomeApp
     feedContent={feedContent}
     onboardingResult={onboardingResult}
