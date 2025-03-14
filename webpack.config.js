15368c29951bb32b216bc9936c36d5b7f84765b8
Wenbin Fang <wenbin@listennotes.com>
2025-03-14 13:53:41 -0700

    Upgrade to wrangler 4.0.0.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const BundleTracker = require('webpack-bundle-tracker');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 const prod = process.env.NODE_ENV === 'production';
 const devPort = 9000;
